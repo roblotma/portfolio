ca1dc5b858d48920eeee7447d3f2ecc8f572bc8f
roblotma <75797984+roblotma@users.noreply.github.com>
2023-09-04 23:55:28 +0300

    run server

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=int(os.environ.get('PORT', 8050)))
+    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
